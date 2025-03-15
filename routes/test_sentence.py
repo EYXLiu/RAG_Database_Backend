acce6bcd9d20b971606f71743742462c1f37d404
Evan Liu <evanyxliu@gmail.com>
2025-03-15 16:19:18 -0400

    less memory

--- routes/test_sentence.py
+++ routes/test_sentence.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
 from sentence_transformers import SentenceTransformer
 
 router = APIRouter()
 
+class SentenceRequest(BaseModel):
+    text: str
+
 @router.get("/sentence")
-async def get():
-    embd = SentenceTransformer(model_name_or_path='Lajavaness/bilingual-embedding-base', trust_remote_code=True)
-    query = "Yes"
-    query_embd = embd.encode(query)
+async def get(request: SentenceRequest):
+    embd = SentenceTransformer(model_name_or_path='Lajavaness/bilingual-embedding-small', trust_remote_code=True)
+    query_embd = embd.encode(request.text)
     return { "embedding": query_embd.tolist() }
